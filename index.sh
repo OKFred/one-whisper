192225214b0a3a0e9385c7a167339e79c1b4aa79
Zhang Qi <zhangqi@tr-ag.com>
2024-02-06 16:39:40 +0800

    feature-初始化whisper的安装/1.0.1

---
+++ index.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+main() {
+  install_python_3
+  install_pip
+  install_ffmpeg
+  install_pytorch
+  install_openai_and_whisper
+}
+
+main
+
+install_python_3() {
+  sudo apt-get update
+  sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget
+  wget https://www.python.org/ftp/python/3.10.10/Python-3.10.10.tgz
+  tar -xf Python-3.10.10.tgz
+  cd Python-3.10.10
+  ./configure --enable-optimizations
+  make
+  sudo make install
+  cd ..
+  sudo rm -rf Python-3.10.10
+  echo "Python3.10.10已安装"
+}
+
+install_pip() {
+  sudo apt-get install python3-pip
+  echo "pip已安装"
+}
+
+install_ffmpeg() {
+  sudo apt-get install ffmpeg
+  echo "ffmpeg已安装"
+}
+
+install_pytorch() {
+  read -p "是否使用英伟达显卡计算？(y/n)" use_gpu
+  if [ $use_gpu = "y" ]; then
+    pip3 install torch torchvision torchaudio
+  else
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+  fi
+  echo "pytorch已安装"
+}
+
+install_openai_and_whisper() {
+  pip3 install openai
+  pip3 install whisper
+  echo "openai和whisper已安装"
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+### @description: 用于 部署语音识别项目的 debian 系统初始化脚本
+
+### @author: Fred Zhang Qi
+
+### @datetime: 2024-02-06
+
+## 运行方法
+
+### 面向过程的方法
+
+`cd $HOME/one-whisper && chmod +x index.sh && ./index.sh`
