d09928e9e1286792717f16edacefea90cbbc97e0
Zhang Qi <zhangqi@tr-ag.com>
2024-02-29 11:20:34 +0800

    fix-python新版本安装优化/1.1.5

--- index.sh
+++ index.sh
@@ -8,17 +8,21 @@
 
 install_python_3() {
   uninstall_old_python
-  local base_url=http://ftp.cn.debian.org/debian/pool/main/p/python3.10/
+  local base_url
   local package_name
+  base_url=http://ftp.cn.debian.org/debian/pool/main/p/python3.10/
   package_name=libpython3.10-minimal_3.10.13-1_amd64.deb
   downloader $package_name $base_url
   installer $package_name
+  base_url=http://ftp.cn.debian.org/debian/pool/main/p/python3.10/
   package_name=libpython3.10-stdlib_3.10.13-1_amd64.deb
   downloader $package_name $base_url
   installer $package_name
+  base_url=http://ftp.cn.debian.org/debian/pool/main/p/python3.10/
   package_name=python3.10-minimal_3.10.13-1_amd64.deb
   downloader $package_name $base_url
   installer $package_name
+  base_url=http://ftp.cn.debian.org/debian/pool/main/p/python3.10/
   package_name=python3.10_3.10.13-1_amd64.deb
   downloader $package_name $base_url
   installer $package_name
